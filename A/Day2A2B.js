05d93335c04eee948e6249732ddae49dc0df51d6
JohnRC <johbenrivcar@gmail.com>
2023-12-09 15:41:56 +0000

    Day2Complete

--- A/Day2A2B.js
+++ A/Day2A2B.js
@@ -9,6 +9,7 @@ function main(){
     console.log("Starting 2A");
 
     var sumOfPossibleGameIds = 0;
+    var sumOfMinSetPowers = 0;
 
     var data = fs.readFileSync('Day2Input.txt', 'utf8');
     let txt = data.toString();
@@ -22,6 +23,9 @@ function main(){
         // Split the line on the colon
         let gameIsPossible = true;
 
+        // --- Challenge 2B
+        let minSet = { red: 0, blue: 0, green: 0 }
+
         let aa = game.split(": ")
 
         // Get the game number from the LH part
@@ -32,13 +36,13 @@ function main(){
         
         let report = false;
         //if ( gameID%10 == 0) 
-            //report= true;
+        if (gameID == 49) report= true;
 
         if(report) console.log(`=========================\n[${game}]`)
         // Get the list of samples for this game
 
-        samples.every((sample, sampleIx)=>{ 
-            let samplePossible = true;
+        samples.forEach((sample, sampleIx)=>{ 
+            let sampleIsPossible = true;
 
             // Split up the entries for each colour in this sample
             let allColours = sample.split(", ");
@@ -46,20 +50,28 @@ function main(){
             if(report) console.log(`Sample ${sampleIx}:`, `[${sample}]`)
 
             // Now check the counts
-            allColours.every( (oneColour, colourIx)=>{
+            allColours.forEach( (oneColour, colourIx)=>{
                 
                 if(report) console.log(`  ${colourIx}: [${oneColour}]`)
-                let [count, hue]=oneColour.split(" ");
+                let [sCount, hue]=oneColour.split(" ");
+                let count = parseInt(sCount);
                 let poss = colourLimits[hue] < count? false : true;
                 
-                if(!poss) samplePossible = false;
+                if(!poss) sampleIsPossible = false;
                 if(!poss) gameIsPossible = false;
 
+                if( count > minSet[hue] ) minSet[hue] = count;
+
+                // --- Challenge 2B
+                if(report){
+                    console.log(` The count for ${hue} is ${count} and the current min is`, minSet)
+                }
 
                 if(report || !poss )  console.log( `     ${hue} count ${count}/${colourLimits[hue] } Possible? ${ (poss? "Yes" : "No" )}`);
-                return samplePossible;
+                return sampleIsPossible;
             })
-            return samplePossible;
+
+            return sampleIsPossible;
         })
 
         if(gameIsPossible) {
@@ -67,10 +79,17 @@ function main(){
         }
 
         console.log(`Game #${gameID} is ${ gameIsPossible? ``: `not ` }possible, sum of possible game IDs is ${ gameIsPossible? `now`: `still` } ${sumOfPossibleGameIds}`);
+        console.log(` The miniumum set for this game was:`, minSet );
+        let setPower = minSet.red * minSet.green * minSet.blue;
+        sumOfMinSetPowers+= setPower;
+        console.log(` The minimum set power is ${setPower} so the total of all of them is now ${sumOfMinSetPowers}`);
     })
 
-    console.log(`Conclusion: Sum of all possible game IDs is ${sumOfPossibleGameIds}`)
+    console.log(`\nConclusion: Sum of all possible game IDs is ${sumOfPossibleGameIds}`);
+    console.log(`            The final total of all of minimum set powers is ${sumOfMinSetPowers}`);
     
+
+
     console.log("Finished 2A");
 }
 main();
